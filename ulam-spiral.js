b83af7127eca81b0d88d4cb39946bf035e0313e2
Rafal Ostrowidzki <r.ostrowidzki@gmail.com>
2020-12-31 14:04:36 +0100

    fix bug where spiral were drawn from left to down instead of from left to up

--- ulam-spiral.js
+++ ulam-spiral.js
@@ -1,31 +1,36 @@
 function translateRight(pointer) {
-  pointer.x++;
+  pointer.x += pixelWidth;
   pointer.number++;
 }
 
 function translateUp(pointer) {
-  pointer.y++;
+  pointer.y -= pixelHeight;
   pointer.number++;
 }
 
 function translateLeft(pointer) {
-  pointer.x--;
+  pointer.x -= pixelWidth;
   pointer.number++;
 }
 
 function translateDown(pointer) {
-  pointer.y--;
+  pointer.y += pixelHeight;
   pointer.number++;
 }
 
 function isPrime(number) {
-  for ( var i = 2; i < number; i++ ) {
-    if ( number % i == 0 ) {
+  for ( var i = 2; i <= number / 2; i++ ) {
+    if ( number % i === 0 ) {
       return false;
     }
   }
   return true;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// SCRIPT ////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 
 const canvas = $("#prime-container")[0];
 const ctx = canvas.getContext('2d');
@@ -33,10 +38,10 @@ const ctx = canvas.getContext('2d');
 // x and y axis in Cartesian coordinate system
 
 // width and height of the pixel in this case - 1px
-const pixelX = 1;
-const pixelY = 1;
+const pixelWidth = 2;
+const pixelHeight = 2;
 // set number of pixels on the board
-const totalPixels = canvas.width * canvas.height;
+const totalPixels = (canvas.width * canvas.height) / (pixelWidth * pixelHeight);
 
 
 // set color of the pixels
@@ -48,25 +53,24 @@ pointer = {
   number: 1 // determine which number is currently represents
 }
 
-// draw first pixel
-ctx.fillRect(pointer.x, pointer.y, pixelX, pixelY);
 // start seeking for prime numbers > 1
-var spiralLength = 1
+var spiralLength = 1;
+totalPrimes = 0;
 while ( pointer.number < totalPixels ) {
 
   for ( var i = 0; i < spiralLength; i ++) {
     translateRight(pointer);
     if ( isPrime(pointer.number) ) {
-      ctx.fillRect(pointer.x, pointer.y, pixelX, pixelY);
-
+      ctx.fillRect(pointer.x, pointer.y, pixelWidth, pixelHeight);
+      totalPrimes++;
     }
   }
 
   for ( var i = 0; i < spiralLength; i ++) {
     translateUp(pointer);
     if ( isPrime(pointer.number) ) {
-      ctx.fillRect(pointer.x, pointer.y, pixelX, pixelY);
-      ctx.save();
+      ctx.fillRect(pointer.x, pointer.y, pixelWidth, pixelHeight);
+      totalPrimes++;
     }
   }
   spiralLength++;
@@ -74,15 +78,19 @@ while ( pointer.number < totalPixels ) {
   for ( var i = 0; i < spiralLength; i ++) {
     translateLeft(pointer);
     if ( isPrime(pointer.number) ) {
-      ctx.fillRect(pointer.x, pointer.y, pixelX, pixelY);
+      ctx.fillRect(pointer.x, pointer.y, pixelWidth, pixelHeight);
+      totalPrimes++;
     }
   }
 
   for ( var i = 0; i < spiralLength; i ++) {
     translateDown(pointer);
     if ( isPrime(pointer.number) ) {
-      ctx.fillRect(pointer.x, pointer.y, pixelX, pixelY);
+      ctx.fillRect(pointer.x, pointer.y, pixelWidth, pixelHeight);
+      totalPrimes++;
     }
   }
   spiralLength++;
 }
+
+console.log("Total primes: " + totalPrimes + ", among " + pointer.number + " numbers.");
