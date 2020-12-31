d111820a7fa2f1f2fa92ed925285652fd288a09b
Rafal Ostrowidzki <r.ostrowidzki@gmail.com>
2020-12-31 12:25:56 +0100

    add flow chart

--- ulam-spiral.js
+++ ulam-spiral.js
@@ -1,5 +1,5 @@
 
-const ctx = document.getElementById('prime-container').getContext('2d');
+const ctx = $("#prime-container")[0].getContext('2d');
 
 ctx.fillStyle = 'black';
-ctx.fillRect(100, 50, 1, 1);
+ctx.fillRect(100, 50, 2, 2);
